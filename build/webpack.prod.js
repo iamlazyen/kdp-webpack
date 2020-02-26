07b121e2ada6f9ed0a7fbbb37e7fb61788424e18
depeng <kongdepeng520@gmail.com>
2020-02-26 16:06:43 +0800

    build: 优化代码压缩时间

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,6 +4,8 @@ const WebpackMerge = require('webpack-merge')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
+
 module.exports = WebpackMerge(webpackConfig,{
   mode:'production',
   devtool:'cheap-module-source-map',
@@ -15,12 +17,21 @@ module.exports = WebpackMerge(webpackConfig,{
   ],
   optimization:{
     minimizer:[
-      new UglifyJsPlugin({//压缩js
-        cache:true,
-        parallel:true,
-        sourceMap:true
-    }),
-    new OptimizeCssAssetsPlugin({})
+      new ParallelUglifyPlugin({
+        cacheDir: '.cache/',
+        uglifyJS: {
+          output: {
+            comments: false,
+            beautify: false
+          },
+          compress: {
+            drop_console: false,
+            collapse_vars: true,
+            reduce_vars: true
+          }
+        }
+      }),
+      new OptimizeCssAssetsPlugin({})
     ],
     splitChunks:{
       chunks:'all',
