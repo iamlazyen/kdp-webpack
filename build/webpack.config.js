3d52f7606bb3d9a095d65a5f923d690413acb306
depeng <kongdepeng520@gmail.com>
2020-02-26 15:30:10 +0800

    build: 处理静态文件及路径

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -28,7 +28,58 @@ module.exports = {
           'postcss-loader', 
           'less-loader'
         ]
-      }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i, //图片文件
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'media/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'fonts/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
     ],
   },
   plugins: [
