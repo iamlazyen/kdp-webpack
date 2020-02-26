186ae42b5a397941f49350928a5925f81f813a6a
depeng <kongdepeng520@gmail.com>
2020-02-26 15:49:08 +0800

    build: 多环境设置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,87 +1,20 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const vueLoaderPlugin = require('vue-loader/lib/plugin')
-const Webpack = require('webpack')
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const devMode = process.argv.indexOf('--mode=production') === -1;
 module.exports = {
-  mode: 'development',
-  entry: ["@babel/polyfill", path.resolve(__dirname, '../src/main.js')],
-  output: {
-    filename: '[name].[hash:8].js',
-    path: path.resolve(__dirname, '../dist')
+  entry:{
+    main:path.resolve(__dirname,'../src/main.js')
   },
-  module: {
-    rules: [
-      {
-        test:/\.css$/,
-        use:[
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test:/\.less$/,
-        use:[
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader', 
-          'postcss-loader', 
-          'less-loader'
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/i, //图片文件
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10240,
-              fallback: {
-                loader: 'file-loader',
-                options: {
-                    name: 'img/[name].[hash:8].[ext]'
-                }
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10240,
-              fallback: {
-                loader: 'file-loader',
-                options: {
-                  name: 'media/[name].[hash:8].[ext]'
-                }
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10240,
-              fallback: {
-                loader: 'file-loader',
-                options: {
-                  name: 'fonts/[name].[hash:8].[ext]'
-                }
-              }
-            }
-          }
-        ]
-      },
+  output:{
+    path:path.resolve(__dirname,'../dist'),
+    filename:'js/[name].[hash:8].js',
+    chunkFilename:'js/[name].[hash:8].js'
+  },
+  module:{
+    rules:[
       {
         test:/\.js$/,
         use:{
@@ -94,9 +27,91 @@ module.exports = {
       },
       {
         test:/\.vue$/,
-        use:['vue-loader']
+        use:[{
+          loader:'vue-loader',
+          options:{
+            compilerOptions:{
+              preserveWhitespace:false
+            }
+          }
+        }]
+      },
+      {
+        test:/\.css$/,
+        use:[{
+          loader: devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          options:{
+            publicPath:"../dist/css/",
+            hmr:devMode
+          }
+        },'css-loader',{
+          loader:'postcss-loader',
+          options:{
+            plugins:[require('autoprefixer')]
+          }
+        }]
+      },
+      {
+        test:/\.less$/,
+        use:[{
+          loader:devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          options:{
+            publicPath:"../dist/css/",
+            hmr:devMode
+          }
+        },'css-loader','less-loader',{
+          loader:'postcss-loader',
+          options:{
+            plugins:[require('autoprefixer')]
+          }
+        }]
+      },
+      {
+        test:/\.(jep?g|png|gif)$/,
+        use:{
+          loader:'url-loader',
+          options:{
+            limit:10240,
+            fallback:{
+              loader:'file-loader',
+              options:{
+                name:'img/[name].[hash:8].[ext]'
+              }
+            }
+          }
+        }
+      },
+      {
+        test:/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use:{
+          loader:'url-loader',
+          options:{
+            limit:10240,
+            fallback:{
+              loader:'file-loader',
+              options:{
+                name:'media/[name].[hash:8].[ext]'
+              }
+            }
+          }
+        }
+      },
+      {
+        test:/\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use:{
+          loader:'url-loader',
+          options:{
+            limit:10240,
+            fallback:{
+              loader:'file-loader',
+              options:{
+                name:'media/[name].[hash:8].[ext]'
+              }
+            }
+          }
+        }
       }
-    ],
+    ]
   },
   resolve:{
     alias:{
@@ -105,21 +120,15 @@ module.exports = {
     },
     extensions:['*','.js','.json','.vue']
   },
-  devServer:{
-    port:3000,
-    hot:true,
-    contentBase:'../dist'
-  },
-  plugins: [
-    new Webpack.HotModuleReplacementPlugin(),
-    new vueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html')
-    }),
+  plugins:[
     new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template:path.resolve(__dirname,'../public/index.html')
+    }),
+    new vueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].[hash].css",
-      chunkFilename: "[id].css",
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
     })
   ]
 }
